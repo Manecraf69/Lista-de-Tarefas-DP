788eb357786bb0ae27325043ceaf75c1e15ccadd
Manecraf69 <marcioviniciusvoss@gmail.com>
2022-12-08 23:38:57 -0300

    personalizando a app bar

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="mb-3">Porque usar uma lista?</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+      pariatur, ullam nisi temporibus harum obcaecati est quos debitis sequi
+      animi explicabo vel vero labore amet placeat porro. Hic, assumenda
+      architecto? Non dignissimos magnam totam, nam recusandae, incidunt odio
+      quo fugit necessitatibus ad inventore. Reprehenderit ipsum iusto, pariatur
+      dolor necessitatibus vero dolore provident adipisci harum recusandae
+      facere omnis at corrupti voluptates?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+      pariatur, ullam nisi temporibus harum obcaecati est quos debitis sequi
+      animi explicabo vel vero labore amet placeat porro. Hic, assumenda
+      architecto? Non dignissimos magnam totam, nam recusandae, incidunt odio
+      quo fugit necessitatibus ad inventore. Reprehenderit ipsum iusto, pariatur
+      dolor necessitatibus vero dolore provident adipisci harum recusandae
+      facere omnis at corrupti voluptates?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+      pariatur, ullam nisi temporibus harum obcaecati est quos debitis sequi
+      animi explicabo vel vero labore amet placeat porro. Hic, assumenda
+      architecto? Non dignissimos magnam totam, nam recusandae, incidunt odio
+      quo fugit necessitatibus ad inventore. Reprehenderit ipsum iusto, pariatur
+      dolor necessitatibus vero dolore provident adipisci harum recusandae
+      facere omnis at corrupti voluptates?
+    </p>
   </div>
 </template>
